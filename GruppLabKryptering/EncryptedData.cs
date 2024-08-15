fa58a356075e59af83febc1c81e30f4c8d8a32b8
kevinmartiin <nextgenallu@hotmail.com>
2024-08-15 13:41:38 +0200

    Installed NuGet packages to support sqlite database. Created a class to represent data i want to save in database. Created a class to represent the database and contains DbSet for the datamodel i created

---
+++ GruppLabKryptering/EncryptedData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppLabKryptering
+{
+    public class EncryptedData
+    {
+        public int Id { get; set; }  // Unikt ID för varje post
+        public string EncryptedText { get; set; }  // Krypterad text
+        public byte[] Salt { get; set; }  // Saltet som används för kryptering
+    }
+}
